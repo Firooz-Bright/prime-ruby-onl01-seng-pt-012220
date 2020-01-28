6b896cd8e6d4aa7a2d290b90fd584c12bda09919
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2020-01-28 23:16:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime (n)
+	is_prime = true
+	if n==2 
+	  return is_prime true 
+	else
+	for i in 2..n-1
+		if n%i==0
+			is_prime=false 
+		end 
+	end 
+	return is_prime
+end 
+
